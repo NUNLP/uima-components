115d6244db3c8cab79638fb38e997a92f976a97c
wthompso <wthompso@R1001-LT-100-F.enhnet.org>
2013-11-27 22:59:07 -0600

    cleaned up working version

--- annotator-sentence/src/main/groovy/org/northshore/cbri/SentenceDetector.java
+++ annotator-sentence/src/main/groovy/org/northshore/cbri/SentenceDetector.java
@@ -18,27 +18,23 @@
  */
 package org.northshore.cbri;
 
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Set;
-import java.util.logging.Logger;
 
 import opennlp.tools.sentdetect.DefaultSDContextGenerator;
 import opennlp.tools.sentdetect.SentenceModel;
-import opennlp.tools.util.InvalidFormatException;
+import opennlp.uima.sentdetect.SentenceModelResource;
 
 import org.apache.ctakes.typesystem.type.textspan.Segment;
 import org.apache.ctakes.typesystem.type.textspan.Sentence;
 import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.analysis_engine.annotator.AnnotatorProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.JFSIndexRepository;
-import org.apache.uima.resource.ResourceAccessException;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import com.google.common.io.Resources;
@@ -49,273 +45,141 @@ import com.google.common.io.Resources;
  * @author Mayo Clinic
  */
 public class SentenceDetector extends JCasAnnotator_ImplBase {
-	/**
-	 * Value is "SegmentsToSkip". This parameter specifies which sections to
-	 * skip. The parameter should be of type String, should be multi-valued and
-	 * optional.
-	 */
-	public static final String PARAM_SEGMENTS_TO_SKIP = "SegmentsToSkip";
 
-	// LOG4J logger based on class name
-	private Logger logger = Logger.getLogger(getClass().getName());
+    public static final String SD_MODEL_FILE_PARAM = "sentenceModelFile";
 
-	public static final String SD_MODEL_FILE_PARAM = "SentenceModelFile";
+    @ConfigurationParameter(name = SD_MODEL_FILE_PARAM, mandatory = true, description = "File holding sentence model")
+    private String sentenceModelFile;
 
-	private opennlp.tools.sentdetect.SentenceModel sdmodel;
+    private opennlp.tools.sentdetect.SentenceModel sdmodel;
+    private SentenceDetectorCtakes sentenceDetector;
 
-	private UimaContext context;
+    @Override
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
 
-	private Set<?> skipSegmentsSet;
+        super.initialize(aContext);
 
-	private SentenceDetectorCtakes sentenceDetector;
+        try {
+            InputStream modelIn = Resources.getResource(sentenceModelFile).openStream();
+            this.sdmodel = new SentenceModel(modelIn);
+            EndOfSentenceScannerImpl eoss = new EndOfSentenceScannerImpl();
+            char[] eosc = eoss.getEndOfSentenceCharacters();
+            DefaultSDContextGenerator cg = new DefaultSDContextGenerator(eosc);
+            sentenceDetector = new SentenceDetectorCtakes(sdmodel.getMaxentModel(), cg, eoss);
+        } catch (Exception ace) {
+            throw new ResourceInitializationException(ace);
+        }
+    }
 
-	private String NEWLINE = "\n";
+    /**
+     * Entry point for processing.
+     */
+    public void process(JCas jcas) throws AnalysisEngineProcessException {
 
-	private int sentenceCount = 0;
+        int sentenceCount = 0;
 
-	public void initialize(UimaContext aContext)
-			throws ResourceInitializationException {
+        String text = jcas.getDocumentText();
 
-		super.initialize(aContext);
+        ////(new Segment(jcas, 0, text.length())).addToIndexes();
+        JFSIndexRepository indexes = jcas.getJFSIndexRepository();
+        Iterator<?> sectionItr = indexes.getAnnotationIndex(Segment.type).iterator();
+        while (sectionItr.hasNext()) {
+            Segment sa = (Segment) sectionItr.next();
+            sentenceCount = annotateRange(jcas, text, sa, sentenceCount);
+        }
+    }
 
-		context = aContext;
-		try {
-			configInit();
-		} catch (Exception ace) {
-			throw new ResourceInitializationException(ace);
-		}
-	}
+    /**
+     * Detect sentences within a section of the text and add annotations to the
+     * CAS. Uses OpenNLP sentence detector, and then additionally forces
+     * sentences to end at end-of-line characters (splitting into multiple
+     * sentences). Also trims sentences. And if the sentence detector does
+     * happen to form a sentence that is just white space, it will be ignored.
+     * 
+     * @param jcas
+     *            view of the CAS containing the text to run sentence detector
+     *            against
+     * @param text
+     *            the document text
+     * @param section
+     *            the section this sentence is in
+     * @param sentenceCount
+     *            the number of sentences added already to the CAS (if
+     *            processing one section at a time)
+     * @return count The sum of <code>sentenceCount</code> and the number of
+     *         Sentence annotations added to the CAS for this section
+     * @throws AnnotatorProcessException
+     */
+    protected int annotateRange(JCas jcas, String text, Segment section, int sentenceCount)
+            throws AnalysisEngineProcessException {
 
-	/**
-	 * Reads configuration parameters.
-	 * 
-	 * @throws ResourceAccessException
-	 * @throws IOException 
-	 * @throws InvalidFormatException 
-	 */
-	private void configInit() throws ResourceAccessException, InvalidFormatException, IOException {
+        int b = section.getBegin();
+        int e = section.getEnd();
 
-		String sdModelPath = (String) context
-				.getConfigParameterValue(SD_MODEL_FILE_PARAM);
-//			InputStream is = FileLocator.getAsStream(sdModelPath);
-		    InputStream is = Resources.getResource(sdModelPath).openStream();
-			logger.info("Sentence detector model file: " + sdModelPath);
-			sdmodel = new SentenceModel(is);
-			is.close();
-			EndOfSentenceScannerImpl eoss = new EndOfSentenceScannerImpl();
-			char[] eosc = eoss.getEndOfSentenceCharacters();
-			// SentenceDContextGenerator cg = new SentenceDContextGenerator();
-			DefaultSDContextGenerator cg = new DefaultSDContextGenerator(eosc);
-			sentenceDetector = new SentenceDetectorCtakes(
-					sdmodel.getMaxentModel(), cg, eoss);
+        // Use OpenNLP tools to split text into sentences
+        // The sentence detector returns the offsets of the sentence-endings it
+        // detects within the string
+        int[] sentenceBreaks = sentenceDetector.sentPosDetect(text.substring(b, e));
 
-//			skipSegmentsSet = ParamUtil.getStringParameterValuesSet(
-//					PARAM_SEGMENTS_TO_SKIP, context);
-	}
+        int numSentences = sentenceBreaks.length;
+        // There might be text after the last sentence-ending found by detector,
+        // so +1
+        SentenceSpan[] potentialSentSpans = new SentenceSpan[numSentences + 1];
 
-	/**
-	 * Entry point for processing.
-	 */
-	public void process(JCas jcas) throws AnalysisEngineProcessException {
+        int sentStart = b;
+        int sentEnd = b;
+        // Start by filling in sentence spans from what OpenNLP tools detected
+        // Will trim leading or trailing whitespace when check for end-of-line
+        // characters
+        for (int i = 0; i < numSentences; i++) {
+            sentEnd = sentenceBreaks[i] + b;
+            String coveredText = text.substring(sentStart, sentEnd);
+            potentialSentSpans[i] = new SentenceSpan(sentStart, sentEnd, coveredText);
+            sentStart = sentEnd;
+        }
 
-		logger.info("Starting processing.");
+        // If detector didn't find any sentence-endings,
+        // or there was text after the last sentence-ending found,
+        // create a sentence from what's left, as long as it's not all
+        // whitespace.
+        // Will trim leading or trailing whitespace when check for end-of-line
+        // characters
+        if (sentEnd < e) {
+            String coveredText = text.substring(sentEnd, e);
+            if (coveredText.trim() != "") {
+                potentialSentSpans[numSentences] = new SentenceSpan(sentEnd, e, coveredText);
+                numSentences++;
+            }
+        }
 
-		sentenceCount = 0;
+        // Copy potentialSentSpans into sentenceSpans,
+        // ignoring any that are entirely whitespace,
+        // trimming the rest,
+        // and splitting any of those that contain an end-of-line character.
+        // Then trim any leading or trailing whitespace of ones that were split.
+        ArrayList<SentenceSpan> sentenceSpans = new ArrayList<SentenceSpan>(0);
+        for (int i = 0; i < potentialSentSpans.length; i++) {
+            if (potentialSentSpans[i] != null) {
+                sentenceSpans.addAll(potentialSentSpans[i].splitAtLineBreaksAndTrim("\n"));
+            }
+        }
 
-		String text = jcas.getDocumentText();
-
-//		Segment segment = new Segment(jcas, 0, text.length());
-//		segment.addToIndexes();
-		JFSIndexRepository indexes = jcas.getJFSIndexRepository();
-		Iterator<?> sectionItr = indexes.getAnnotationIndex(Segment.type)
-				.iterator();
-		while (sectionItr.hasNext()) {
-			Segment sa = (Segment) sectionItr.next();
-			String sectionID = sa.getId();
-			if (true) {
-				sentenceCount = annotateRange(jcas, text, sa, sentenceCount);
-			}
-		}
-	}
-
-	/**
-	 * Detect sentences within a section of the text and add annotations to the
-	 * CAS. Uses OpenNLP sentence detector, and then additionally forces
-	 * sentences to end at end-of-line characters (splitting into multiple
-	 * sentences). Also trims sentences. And if the sentence detector does
-	 * happen to form a sentence that is just white space, it will be ignored.
-	 * 
-	 * @param jcas
-	 *            view of the CAS containing the text to run sentence detector
-	 *            against
-	 * @param text
-	 *            the document text
-	 * @param section
-	 *            the section this sentence is in
-	 * @param sentenceCount
-	 *            the number of sentences added already to the CAS (if
-	 *            processing one section at a time)
-	 * @return count The sum of <code>sentenceCount</code> and the number of
-	 *         Sentence annotations added to the CAS for this section
-	 * @throws AnnotatorProcessException
-	 */
-	protected int annotateRange(JCas jcas, String text, Segment section,
-			int sentenceCount) throws AnalysisEngineProcessException {
-
-		int b = section.getBegin();
-		int e = section.getEnd();
-
-		// Use OpenNLP tools to split text into sentences
-		// The sentence detector returns the offsets of the sentence-endings it
-		// detects
-		// within the string
-		int[] sentenceBreaks = sentenceDetector.sentPosDetect(text.substring(b,
-				e)); // OpenNLP tools 1.5 returns Spans rather than offsets that
-						// 1.4 did
-		int numSentences = sentenceBreaks.length;
-		// There might be text after the last sentence-ending found by detector,
-		// so +1
-		SentenceSpan[] potentialSentSpans = new SentenceSpan[numSentences + 1];
-
-		int sentStart = b;
-		int sentEnd = b;
-		// Start by filling in sentence spans from what OpenNLP tools detected
-		// Will trim leading or trailing whitespace when check for end-of-line
-		// characters
-		for (int i = 0; i < numSentences; i++) {
-			sentEnd = sentenceBreaks[i] + b; // OpenNLP tools 1.5 returns Spans
-												// rather than offsets that 1.4
-												// did
-			String coveredText = text.substring(sentStart, sentEnd);
-			potentialSentSpans[i] = new SentenceSpan(sentStart, sentEnd,
-					coveredText);
-			sentStart = sentEnd;
-		}
-
-		// If detector didn't find any sentence-endings,
-		// or there was text after the last sentence-ending found,
-		// create a sentence from what's left, as long as it's not all
-		// whitespace.
-		// Will trim leading or trailing whitespace when check for end-of-line
-		// characters
-		if (sentEnd < e) {
-			String coveredText = text.substring(sentEnd, e);
-			if (coveredText.trim() != "") {
-				potentialSentSpans[numSentences] = new SentenceSpan(sentEnd, e,
-						coveredText);
-				numSentences++;
-			}
-		}
-
-		// Copy potentialSentSpans into sentenceSpans,
-		// ignoring any that are entirely whitespace,
-		// trimming the rest,
-		// and splitting any of those that contain an end-of-line character.
-		// Then trim any leading or trailing whitespace of ones that were split.
-		ArrayList<SentenceSpan> sentenceSpans = new ArrayList<SentenceSpan>(0);
-		for (int i = 0; i < potentialSentSpans.length; i++) {
-			if (potentialSentSpans[i] != null) {
-				sentenceSpans.addAll(potentialSentSpans[i]
-						.splitAtLineBreaksAndTrim(NEWLINE)); // TODO Determine
-																// line break
-																// type
-			}
-		}
-
-		// Add sentence annotations to the CAS
-		int previousEnd = -1;
-		for (int i = 0; i < sentenceSpans.size(); i++) {
-			SentenceSpan span = sentenceSpans.get(i);
-			if (span.getStart() != span.getEnd()) { // skip empty lines
-				Sentence sa = new Sentence(jcas);
-				sa.setBegin(span.getStart());
-				sa.setEnd(span.getEnd());
-				if (previousEnd <= sa.getBegin()) {
-					// System.out.println("Adding Sentence Annotation for " +
-					// span.toString());
-					sa.setSentenceNumber(sentenceCount);
-					sa.addToIndexes();
-					sentenceCount++;
-					previousEnd = span.getEnd();
-				}
-			}
-		}
-		return sentenceCount;
-	}
-
-	/**
-	 * Train a new sentence detector from the training data in the first file
-	 * and write the model to the second file.<br>
-	 * The training data file is expected to have one sentence per line.
-	 * 
-	 * @param args
-	 *            training_data_filename name_of_model_to_create iters? cutoff?
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		final Logger logger = Logger.getLogger(SentenceDetector.class.getName()
-				+ ".main()");
-
-		// Handle arguments
-		if (args.length < 2 || args.length > 4) {
-			usage(logger);
-			System.exit(-1);
-		}
-
-		File inFile = getReadableFile(args[0]);
-
-		File outFile = getFileInExistingDir(args[1]);
-		// File outFile = new File(args[1]);
-
-		int iters = 100;
-		if (args.length > 2) {
-			iters = parseInt(args[2], logger);
-		}
-
-		int cut = 5;
-		if (args.length > 3) {
-			cut = parseInt(args[3], logger);
-		}
-
-		// Now, do the actual training
-		EndOfSentenceScannerImpl scanner = new EndOfSentenceScannerImpl();
-		int numEosc = scanner.getEndOfSentenceCharacters().length;
-
-		logger.info("Training new model from " + inFile.getAbsolutePath());
-		logger.info("Using " + numEosc + " end of sentence characters.");
-
-	}
-
-	public static void usage(Logger log) {
-		log.info("Usage: java "
-				+ SentenceDetector.class.getName()
-				+ " training_data_filename name_of_model_to_create <iters> <cut>");
-	}
-
-	public static int parseInt(String s, Logger log) {
-		try {
-			return Integer.parseInt(s);
-		} catch (NumberFormatException nfe) {
-			throw (nfe);
-		}
-	}
-
-	public static File getReadableFile(String fn) throws IOException {
-		File f = new File(fn);
-		if (!f.canRead()) {
-			throw new IOException("Unable to read from file "
-					+ f.getAbsolutePath());
-		}
-		return f;
-	}
-
-	public static File getFileInExistingDir(String fn) throws IOException {
-		File f = new File(fn);
-		if (!f.getParentFile().isDirectory()) {
-			throw new IOException("Directory not found: "
-					+ f.getParentFile().getAbsolutePath());
-		}
-		return f;
-	}
-
+        // Add sentence annotations to the CAS
+        int previousEnd = -1;
+        for (int i = 0; i < sentenceSpans.size(); i++) {
+            SentenceSpan span = sentenceSpans.get(i);
+            if (span.getStart() != span.getEnd()) { // skip empty lines
+                Sentence sa = new Sentence(jcas);
+                sa.setBegin(span.getStart());
+                sa.setEnd(span.getEnd());
+                if (previousEnd <= sa.getBegin()) {
+                    sa.setSentenceNumber(sentenceCount);
+                    sa.addToIndexes();
+                    sentenceCount++;
+                    previousEnd = span.getEnd();
+                }
+            }
+        }
+        return sentenceCount;
+    }
 }
