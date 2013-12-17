21b4594d99700fdaf9ab210169b1e867b1177bf6
wthompso <wthompso@R1001-LT-100-F.enhnet.org>
2013-12-17 15:28:33 -0600

    update to use LexNormalizer

--- aggregate-test/src/test/groovy/org/northshore/cbri/AnnotatorTest.groovy
+++ aggregate-test/src/test/groovy/org/northshore/cbri/AnnotatorTest.groovy
@@ -68,6 +68,10 @@ class AnnotatorTest {
                 "opennlp.uima.POSFeature", "partOfSpeech")
         ExternalResourceFactory.createDependencyAndBind(posTagger, UimaUtil.MODEL_PARAMETER,
                 opennlp.uima.postag.POSModelResourceImpl, "file:models/mayo-pos.zip")
+        
+        // Lex normalizer
+        AnalysisEngineDescription lexNormalizer = AnalysisEngineFactory.createEngineDescription(LexNormalizerAnnotator,
+            LexNormalizerAnnotator.PARAM_LEXICON_NAME, "2006Lexicon")
 
         // Build the aggregate
         AggregateBuilder builder = new AggregateBuilder()
@@ -75,7 +79,8 @@ class AnnotatorTest {
         builder.add(sentDetector)
         builder.add(tokenizer)
         builder.add(posTagger)
-
+        builder.add(lexNormalizer)
+        
         AnalysisEngineDescription desc = builder.createAggregateDescription()
         ////PrintWriter writer = new PrintWriter(new File("src/test/resources/descriptors/TestAggregateUIMAFit.xml"))
         ////desc.toXML(writer)
