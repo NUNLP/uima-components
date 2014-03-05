09370b801b63116b947f90f69ca62bc38796e423
wthompso <wthompso@R1001-LT-100-F.enhnet.org>
2014-03-05 17:10:25 -0600

    updates

--- annotator-parent/src/test/groovy/org/northshore/cbri/AnnotatorTest.groovy
+++ annotator-parent/src/test/groovy/org/northshore/cbri/AnnotatorTest.groovy
@@ -88,4 +88,49 @@ class AnnotatorTest {
 
         AnalysisEngine engine = builder.createAggregate()
     }
+    
+    static void main(args) {
+        // Type system
+        TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription()
+        tsd.resolveImports()
+        
+        // Segmenter
+        AnalysisEngineDescription segmenter = AnalysisEngineFactory.createEngineDescription(GroovyAnnotator,
+                tsd, GroovyAnnotator.PARAM_SCRIPT_FILE, "groovy/SimpleSegmenter.groovy")
+        // Sentence detector
+        AnalysisEngineDescription sentDetector = AnalysisEngineFactory.createEngineDescription(
+                SentenceDetector_cTAKES,
+                SentenceDetector_cTAKES.SD_MODEL_FILE_PARAM, "models/sd-med-model.zip")
+        // Tokenizer
+        AnalysisEngineDescription tokenizer = AnalysisEngineFactory.createEngineDescription(
+                TokenAnnotator)
+        ExternalResourceFactory.createDependencyAndBind(tokenizer,
+                TokenAnnotator.TOKEN_MODEL_KEY,
+                opennlp.uima.tokenize.TokenizerModelResourceImpl,
+                "file:models/en-token.bin")
+        // POS tagger
+        AnalysisEngineDescription posTagger = AnalysisEngineFactory.createEngineDescription(
+                opennlp.uima.postag.POSTagger,
+                "opennlp.uima.SentenceType", Sentence.name,
+                "opennlp.uima.TokenType", BaseToken.name,
+                "opennlp.uima.POSFeature", "partOfSpeech")
+        ExternalResourceFactory.createDependencyAndBind(posTagger, UimaUtil.MODEL_PARAMETER,
+                opennlp.uima.postag.POSModelResourceImpl, "file:models/mayo-pos.zip")      
+        // Lex normalizer
+//        AnalysisEngineDescription lexNormalizer = AnalysisEngineFactory.createEngineDescription(LexNormalizerAnnotator,
+//            LexNormalizerAnnotator.PARAM_LEXICON_NAME, "2006Lexicon")
+
+        // Build the aggregate
+        AggregateBuilder builder = new AggregateBuilder()
+        builder.add(segmenter)
+        builder.add(sentDetector)
+        builder.add(tokenizer)
+        builder.add(posTagger)
+//        builder.add(lexNormalizer)
+        
+        AnalysisEngineDescription desc = builder.createAggregateDescription()
+        PrintWriter writer = new PrintWriter(new File("src/test/resources/descriptors/TestAggregateUIMAFit.xml"))
+        desc.toXML(writer)
+        writer.close()
+    }
 }
