e9e1a34103965de26381cac207cda6673d516235
wthompso <wthompso@R1001-LT-100-F.enhnet.org>
2014-05-07 15:19:27 -0500

    adding test

---
+++ annotator-time/src/main/groovy/org/northshore/cbri/TimeExRecognizer.groovy
@@ -0,0 +1,65 @@
+package org.northshore.cbri
+
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.pipeline.Annotation
+import edu.stanford.nlp.pipeline.AnnotationPipeline
+import edu.stanford.nlp.pipeline.POSTaggerAnnotator
+import edu.stanford.nlp.pipeline.PTBTokenizerAnnotator
+import edu.stanford.nlp.pipeline.WordsToSentencesAnnotator
+import edu.stanford.nlp.time.TimeAnnotations
+import edu.stanford.nlp.time.TimeAnnotator
+import edu.stanford.nlp.time.TimeExpression
+import edu.stanford.nlp.util.CoreMap
+import groovy.util.logging.Log4j
+
+import org.apache.ctakes.typesystem.type.textsem.*
+import org.apache.log4j.Level
+import org.apache.uima.UimaContext
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase
+import org.apache.uima.fit.descriptor.ConfigurationParameter
+import org.apache.uima.jcas.JCas
+import org.apache.uima.resource.ResourceInitializationException
+
+@Log4j
+class TimeExRecognizer extends JCasAnnotator_ImplBase {
+    public static final String TIMEREC_ANCHOR_DATE = 'anchorDate'
+    
+    @ConfigurationParameter(name = 'anchorDate', mandatory = false, description = 'Anchor date')
+    private String segmentsToParse
+    
+    private AnnotationPipeline pipeline;
+    
+    @Override
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        super.initialize(aContext)
+        
+        Properties props = new Properties()
+        pipeline = new AnnotationPipeline()
+        pipeline.addAnnotator(new PTBTokenizerAnnotator(false))
+        pipeline.addAnnotator(new WordsToSentencesAnnotator(false))
+        pipeline.addAnnotator(new POSTaggerAnnotator(false))
+        pipeline.addAnnotator(new TimeAnnotator('sutime', props))
+    }
+    
+    @Override
+    public void process(JCas jcas) throws AnalysisEngineProcessException {
+        log.setLevel(Level.INFO)
+        
+        Annotation annotation = new Annotation(jcas.documentText)
+        annotation.set(CoreAnnotations.DocDateAnnotation, '2014-05-06')
+        this.pipeline.annotate(annotation)
+                
+        List timexAnnsAll = annotation.get(TimeAnnotations.TimexAnnotations)
+        timexAnnsAll.each { CoreMap cm ->
+            List tokens = cm.get(CoreAnnotations.TokensAnnotation)
+            TimeMention tm = new TimeMention(jcas)
+            tm.begin = tokens.get(0).get(CoreAnnotations.CharacterOffsetBeginAnnotation)
+            tm.end = tokens.get(tokens.size() - 1).get(CoreAnnotations.CharacterOffsetEndAnnotation)
+//            println cm.toString() + ' [from char offset ' +
+//                    tokens.get(0).get(CoreAnnotations.CharacterOffsetBeginAnnotation) +
+//                    ' to ' + tokens.get(tokens.size() - 1).get(CoreAnnotations.CharacterOffsetEndAnnotation) + ']' +
+//                    ' --> ' + cm.get(TimeExpression.Annotation).getTemporal()
+        }
+    }
+}
---
+++ annotator-time/src/test/groovy/org/northshore/cbri/TimeExRecognizerTest.groovy
@@ -0,0 +1,70 @@
+package org.northshore.cbri;
+
+import static org.junit.Assert.*
+import groovy.util.logging.Log4j
+
+import org.apache.ctakes.typesystem.type.textsem.TimeMention
+import org.apache.log4j.BasicConfigurator
+import org.apache.log4j.Level
+import org.apache.uima.analysis_engine.AnalysisEngine
+import org.apache.uima.analysis_engine.AnalysisEngineDescription
+import org.apache.uima.fit.factory.AnalysisEngineFactory
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory
+import org.apache.uima.jcas.JCas
+import org.apache.uima.resource.metadata.TypeSystemDescription
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.Test
+
+@Log4j
+class TimeExRecognizerTest {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        BasicConfigurator.configure()
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        log.setLevel(Level.INFO)
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void smokeTest() {
+       //build type system description
+        TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription()
+        tsd.resolveImports()
+        
+        // create a sentence detector engine
+        AnalysisEngineDescription desc = AnalysisEngineFactory.createEngineDescription(
+                TimeExRecognizer,
+                TimeExRecognizer.TIMEREC_ANCHOR_DATE, '2014-05-12')
+        AnalysisEngine engine = AnalysisEngineFactory.createEngine(desc)
+        
+        assert engine != null
+        
+        // load in the text to process
+        String text = 'The patient had an onset of diabetes on May 6, 2000. That was 14 years ago.'
+
+        // create a new CAS and seed with a Segment
+        JCas jcas = engine.newJCas()
+        jcas.setDocumentText(text)
+        UIMAUtil.jcas = jcas
+
+        // apply the sentence detector
+        engine.process(jcas)
+        Collection<TimeMention> timeMentions = UIMAUtil.select(type:TimeMention)
+        assert timeMentions.size() == 2
+        timeMentions.each { println "TimeMention: $it.coveredText" }
+    }
+}
