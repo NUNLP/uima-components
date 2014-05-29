56e580f522ebf0a93260d412fb57ca347fb582b9
schorndorfer <will.k.t@gmail.com>
2014-05-29 17:48:34 -0500

    updates

--- annotator-parent/annotator-dictionary-lingpipe/src/test/groovy/org/northshore/cbri/dict/lingpipe/LingPipeDictionaryTest.groovy
+++ annotator-parent/annotator-dictionary-lingpipe/src/test/groovy/org/northshore/cbri/dict/lingpipe/LingPipeDictionaryTest.groovy
@@ -13,11 +13,12 @@ import com.aliasi.chunk.Chunking
 import com.aliasi.dict.DictionaryEntry
 import com.aliasi.dict.ExactDictionaryChunker
 import com.aliasi.dict.MapDictionary
+import com.aliasi.dict.TrieDictionary
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory
 
 class LingPipeDictionaryTest {
     static final double CHUNK_SCORE = 1.0
-    
+
     static String testText = """
 FINAL DIAGNOSIS: 
 A) Ileocecal valve, colon, polyp: 
@@ -51,22 +52,35 @@ C) Sigmoid colon:
         dictionary.addEntry(new DictionaryEntry<String>('adenoma',         'C0206677', CHUNK_SCORE))
         dictionary.addEntry(new DictionaryEntry<String>('tubular adenoma', 'C1112503', CHUNK_SCORE))
         dictionary.addEntry(new DictionaryEntry<String>('sigmoid colon',   'C0227391', CHUNK_SCORE))
-        
+
         ExactDictionaryChunker dictionaryChunkerTT = new ExactDictionaryChunker(dictionary,
-                                     IndoEuropeanTokenizerFactory.INSTANCE,
-                                     true,  // all matches (overlapping)
-                                     false)  // case sensitive
-        
+                IndoEuropeanTokenizerFactory.INSTANCE,
+                true,  // all matches (overlapping)
+                false)  // case sensitive
+
         Chunking chunking = dictionaryChunkerTT.chunk(testText)
         assert chunking.chunkSet().size() == 3
-        
+
         chunking.chunkSet().each { Chunk chunk ->
             int start = chunk.start()
             int end = chunk.end()
             String type = chunk.type()
             double score = chunk.score()
             String phrase = testText.substring(start,end)
-            println "    phrase=|${phrase}| start=${start} end=${end} type=${type} score=${score}"
+            println "phrase=|${phrase}| start=${start} end=${end} type=${type} score=${score}"
         }
+    }
+
+    @Test
+    public void approxDictionaryTest() {
+        DictionaryEntry<String> entry1 = new DictionaryEntry<String>('adenoma', 'C0206677');
+        DictionaryEntry<String> entry2 = new DictionaryEntry<String>('tubular adenoma', 'C1112503vvvv');
+        DictionaryEntry<String> entry3 = new DictionaryEntry<String>('sigmoid colon','C0227391');
+        TrieDictionary<String> dict = new TrieDictionary<String>();
+        dict.addEntry(entry1);
+        dict.addEntry(entry2);
+        dict.addEntry(entry3);
+
+        println "Dictionary: ${dict.toString()}\n"
     }
 }
