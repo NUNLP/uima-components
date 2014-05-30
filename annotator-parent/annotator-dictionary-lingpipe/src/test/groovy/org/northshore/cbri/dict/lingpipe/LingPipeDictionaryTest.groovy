faa0b418a8bd8f500a1f8232e824a156c5e24bd4
schorndorfer <will.k.t@gmail.com>
2014-05-30 11:22:06 -0500

    updates

--- annotator-parent/annotator-dictionary-lingpipe/src/test/groovy/org/northshore/cbri/dict/lingpipe/LingPipeDictionaryTest.groovy
+++ annotator-parent/annotator-dictionary-lingpipe/src/test/groovy/org/northshore/cbri/dict/lingpipe/LingPipeDictionaryTest.groovy
@@ -10,10 +10,13 @@ import org.junit.Test
 
 import com.aliasi.chunk.Chunk
 import com.aliasi.chunk.Chunking
+import com.aliasi.dict.ApproxDictionaryChunker
 import com.aliasi.dict.DictionaryEntry
 import com.aliasi.dict.ExactDictionaryChunker
 import com.aliasi.dict.MapDictionary
 import com.aliasi.dict.TrieDictionary
+import com.aliasi.spell.FixedWeightEditDistance
+import com.aliasi.spell.WeightedEditDistance
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory
 
 class LingPipeDictionaryTest {
@@ -27,7 +30,10 @@ B) Transverse colon polyp:
     - Adenomatous polyp. 
 C) Sigmoid colon: 
     - Hyperplastic polyp. 
-    - Tubular adenoma .
+    - Tubular adenoma.
+D) Sigmod colon: 
+    - Hyperplystic polyp. 
+    - Tubluar adinoma.
     """
 
     @BeforeClass
@@ -49,17 +55,18 @@ C) Sigmoid colon:
     @Test
     public void exactDictionaryTest() {
         MapDictionary<String> dictionary = new MapDictionary<String>()
-        dictionary.addEntry(new DictionaryEntry<String>('adenoma',         'C0206677', CHUNK_SCORE))
-        dictionary.addEntry(new DictionaryEntry<String>('tubular adenoma', 'C1112503', CHUNK_SCORE))
-        dictionary.addEntry(new DictionaryEntry<String>('sigmoid colon',   'C0227391', CHUNK_SCORE))
-
+        dictionary.addEntry(new DictionaryEntry<String>('adenoma',            'C0206677', CHUNK_SCORE))
+        dictionary.addEntry(new DictionaryEntry<String>('tubular adenoma',    'C1112503', CHUNK_SCORE))
+        dictionary.addEntry(new DictionaryEntry<String>('sigmoid colon',      'C0227391', CHUNK_SCORE))
+        dictionary.addEntry(new DictionaryEntry<String>('hyperplastic polyp', 'C0333983', CHUNK_SCORE))
+        
         ExactDictionaryChunker dictionaryChunkerTT = new ExactDictionaryChunker(dictionary,
                 IndoEuropeanTokenizerFactory.INSTANCE,
                 true,  // all matches (overlapping)
                 false)  // case sensitive
 
         Chunking chunking = dictionaryChunkerTT.chunk(testText)
-        assert chunking.chunkSet().size() == 3
+        assert chunking.chunkSet().size() == 4
 
         chunking.chunkSet().each { Chunk chunk ->
             int start = chunk.start()
@@ -73,14 +80,36 @@ C) Sigmoid colon:
 
     @Test
     public void approxDictionaryTest() {
-        DictionaryEntry<String> entry1 = new DictionaryEntry<String>('adenoma', 'C0206677');
-        DictionaryEntry<String> entry2 = new DictionaryEntry<String>('tubular adenoma', 'C1112503vvvv');
-        DictionaryEntry<String> entry3 = new DictionaryEntry<String>('sigmoid colon','C0227391');
-        TrieDictionary<String> dict = new TrieDictionary<String>();
-        dict.addEntry(entry1);
-        dict.addEntry(entry2);
-        dict.addEntry(entry3);
-
+        TrieDictionary<String> dict = new TrieDictionary<String>()
+        dict.addEntry(new DictionaryEntry<String>('adenoma',            'C0206677'))
+        dict.addEntry(new DictionaryEntry<String>('tubular adenoma',    'C1112503'))
+        dict.addEntry(new DictionaryEntry<String>('sigmoid colon',      'C0227391'))
+        dict.addEntry(new DictionaryEntry<String>('hyperplastic polyp', 'C0333983'))
+        
         println "Dictionary: ${dict.toString()}\n"
+
+        WeightedEditDistance editDistance = new FixedWeightEditDistance(0,-1,-1,-1,Double.NaN)
+
+        double maxDistance = 2.0
+
+        ApproxDictionaryChunker chunker = new ApproxDictionaryChunker(dict, IndoEuropeanTokenizerFactory.INSTANCE, editDistance, maxDistance)
+        Chunking chunking = chunker.chunk(testText)
+        CharSequence cs = chunking.charSequence()
+        Set<Chunk> chunkSet = chunking.chunkSet()
+        assert chunkSet.size() == 12
+        
+        printf("%15s  %15s   %8s\n",
+                "Matched Phrase",
+                "Dict Entry",
+                "Distance")
+        chunkSet.each { Chunk chunk ->
+            int start = chunk.start()
+            int end = chunk.end()
+            CharSequence str = cs.subSequence(start,end)
+            double distance = chunk.score()
+            String match = chunk.type()
+            printf("%15s  %15s   %8.1f\n", str, match, distance)
+        }
+        println '\n'
     }
 }
